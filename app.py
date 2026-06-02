946500fffdd0a7e6b366d28395ece64da3d420f0
Swapan <sksarkar2outlook@gmail.com>
2026-06-02 19:32:55 +0530

    fa

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(
+    page_title="RAG Chatbot",
+    page_icon="🤖",
+    layout="wide"
+)
+st.title("📚 PDF RAG Chatbot")
+
+#st.write("Hello, Streamlit Cloud!")
