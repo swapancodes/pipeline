b08f6578a1529cd37b6a820af02c07354237d9dc
swapancodes <sksarkar2outlook@gmail.com>
2026-06-02 20:51:32 +0530

    rag

--- app.py
+++ app.py
@@ -20,12 +20,17 @@ client = get_milvus_client()
 st.title("Milvus Vector Database Connection")
 
 #---------Create a Collection----------
-client.drop_collection(collection_name="demo1_collection")
-
+if client.has_collection(collection_name="rag_collection"):
+    client.drop_collection(collection_name="rag_collection")
+client.create_collection(
+    collection_name="rag_collection",
+    dimension=384,  # The vectors we will use in this demo has 384 dimensions
+)
 # Verify connection by listing available collections
 try:
     collections = client.list_collections()
     st.success("Successfully connected to Milvus!")
     st.write("Available Collections:", collections)
 except Exception as e:
-    st.error(f"Failed to connect to Milvus: {e}")
+    st.error(f"Failed to connect to Milvu
+
