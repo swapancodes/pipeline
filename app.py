8615b1f1d1e0a9c6eaea79fea2faf719896375e2
swapancodes <sksarkar2outlook@gmail.com>
2026-06-03 11:10:34 +0530

    fd

--- app.py
+++ app.py
@@ -6,38 +6,75 @@ st.set_page_config(
 )
 st.title("📚 PDF RAG Chatbot")
 
+from langchain_community.document_loaders import (DirectoryLoader, PyMuPDFLoader)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
 from pymilvus import MilvusClient
-# Cache the connection pool for performance optimization
+COLLECTION_NAME = "demo_collection"
+
+#-----------Milvus Connection-----------
 @st.cache_resource
 def get_milvus_client():
     return MilvusClient(
         uri=st.secrets["MILVUS_URI"],
         token=st.secrets["MILVUS_TOKEN"]
     )
-# Connect to the instance
-client = get_milvus_client()
 
-st.title("Milvus Vector Database Connection")
+#----------Load and Chunk PDFs-------------
+@st.cache_data
+def load_documents():
+    loader = DirectoryLoader(
+        "data/docs",
+        glob="**/*.pdf",
+        loader_cls=PyMuPDFLoader,
+        show_progress=True,
+    )
+    docs = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    chunks = splitter.split_documents(docs)
+    return chunks
 
-#---------Create a Collection----------
-if client.has_collection(collection_name="demo_collection"):
-    client.drop_collection(collection_name="demo_collection")
-client.create_collection(
-    collection_name="demo_collection",
-    dimension=384,  # The vectors we will use in this demo has 384 dimensions
-)
-# Verify connection by listing available collections
+#----------Generate Embeddings---------
+@st.cache_resource
+def get_embedding_model():
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+def build_vector_store():
+    client = get_milvus_client()
+    chunks = load_documents()
+    texts = [chunk.page_content for chunk in chunks]
+    embeddings = get_embedding_model()
+    vectors = embeddings.embed_documents(texts)
+    # Create collection only once
+    if not client.has_collection(COLLECTION_NAME):
+        client.create_collection(
+            collection_name=COLLECTION_NAME,
+            dimension=384
+        )
+        data = []
+        for i, (chunk, vector) in enumerate(zip(chunks, vectors)):
+            data.append(
+                {
+                    "id": i,
+                    "vector": vector,
+                    "text": chunk.page_content,
+                }
+            )
+        client.insert(
+            collection_name=COLLECTION_NAME,
+            data=data
+        )
+
+        st.success(
+            f"Inserted {len(data)} chunks into Milvus."
+        )
+    else:
+        st.info("Collection already exists.")
+
+#---------Main---------
 try:
-    collections = client.list_collections()
-    st.success("Successfully connected to Milvus!")
-    st.write("Available Collections:", collections)
+    build_vector_store()
 except Exception as e:
-    st.error(f"Failed to connect to Milvus")
-
-
-
-
-
-
-
-
+    st.error(f"Error: {str(e)}")
